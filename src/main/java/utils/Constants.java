b5c5ee6de584fc92df8422d9b7a0298f6a06cde1
zyin-08 <zhyin2608@gmail.com>
2026-05-07 01:34:50 +0200

    Replace hardcoded storage strings with Constants

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author zyin2
+ */
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String STORAGE_ARRAY_LIST = "ArrayList";
+    public static final String STORAGE_HASH_MAP = "HashMap";
+    public static final String STORAGE_FILE = "File";
+    public static final String STORAGE_FILE_SERIALIZATION = "File (Serialization)";
+    public static final String STORAGE_SQL_DATABASE = "SQL - Database";
+    public static final String STORAGE_JPA_DATABASE = "JPA - Database";
+
+}
