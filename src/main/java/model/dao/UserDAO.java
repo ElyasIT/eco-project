5112aa75c83288afdf1ed7b0cf5d7180df24d927
Elyas Iqbal Medina <elyaass454@gmail.com>
2026-05-26 09:50:51 +0200

    Filter actions on people menu based on user role #15

---
+++ src/main/java/model/dao/UserDAO.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+import model.entity.User;
+
+/**
+ *
+ * @author zyin2
+ */
+public class UserDAO {
+
+    private static final Map<String, String[]> USERS = new HashMap<>();
+
+    static {
+        USERS.put("User", new String[]{"12345678", "ADMIN"});
+        USERS.put("EMPLOYEE", new String[]{"12345678", "EMPLOYEE"});
+
+    }
+
+    public boolean validate(String username, String password) {
+        if (username == null || username.isEmpty()) {
+            return false;
+        }
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+        return USERS.containsKey(username) && USERS.get(username)[0].equals(password);
+    }
+
+    public boolean isAdmin(String username) {
+        return USERS.containsKey(username) && USERS.get(username)[1].equals("ADMIN");
+    }
+}
